4d5f8810e3343d4fd181e2331026ed52f8f96902
Luka Kavtaradze <Lk@gmail.com>
2024-01-06 01:40:56 +0400

    change hovers

--- src/components/paycancel/paycancel.jsx
+++ src/components/paycancel/paycancel.jsx
@@ -1,16 +1,22 @@
-
-
-
 function PayCancel() {
-    return(
-        <div className="flex flex-col items-center gap-[24px] pt-[24px] pb-[32px]">
-            <button id="pay" className="w-[279px] h-[50px] rounded-[11px] bg-[#382AE1] font-black  text-[white] xl:w-[354px] xl:h-[50px] ;
-]"> Proceed to Payment</button>
-            <button className="text-[15px] text-[#717FA6] font-black"> Cancel Order</button>            
-        </div>
-    )
+  return (
+    <div className="flex flex-col items-center gap-[24px] pt-[24px] pb-[32px]">
+      <button
+        id="pay"
+        className="w-[279px] h-[50px] rounded-[11px] hover: cursor-pointer hover:bg-[#766CF1] bg-[#382AE1] font-black  text-[white] xl:w-[354px] xl:h-[50px] ;
+]"
+      >
+        {" "}
+        Proceed to Payment
+      </button>
+      <button
+        className="text-[15px] hover:cursor-pointer hover:text-[#1F2E55] text-[#717FA6] font-black"
+      >
+        {" "}
+        Cancel Order
+      </button>
+    </div>
+  );
 }
 
-
-
-export default PayCancel
+export default PayCancel;
--- src/components/price/price.jsx
+++ src/components/price/price.jsx
@@ -1,21 +1,28 @@
-import musicLogo from "../../assets/icon-music.svg"                             
-
-
+import musicLogo from "../../assets/icon-music.svg";
 
 function Price() {
-    return(
-        <div id="price" className="flex items-center bg-[#F7F9FF] rounded-[11px] mt-[24px] h-[81px] justify-between w-[279px] xl:w-[354px] xl:h-[98px]">
-            <img src={musicLogo} alt="music-icon"  className="pl-[16px]"/>
-            <div className="flex flex-col pr-[40px]">
-                <span className="text-[#1F2E55] text-[14px] font-black w-[88px] xl:w-[117px]">Annual Plan</span>
-                <span className="w-[81px] font-medium text-[14px] text-[#717FA6] xl:w-[92px]">$59.99/year</span>
-            </div>
-            <a href="" className="text-[#382AE1] text-[13px] font-bold pr-[35px] underline underline-offset-1">Change</a>
-        </div>
-
-    )
+  return (
+    <div
+      id="price"
+      className="flex items-center bg-[#F7F9FF] rounded-[11px] mt-[24px] h-[81px] justify-between w-[279px] xl:w-[354px] xl:h-[98px]"
+    >
+      <img src={musicLogo} alt="music-icon" className="pl-[16px]" />
+      <div className="flex flex-col pr-[40px]">
+        <span className="text-[#1F2E55] text-[14px] font-black w-[88px] xl:w-[117px]">
+          Annual Plan
+        </span>
+        <span className="w-[81px] font-medium text-[14px] text-[#717FA6] xl:w-[92px]">
+          $59.99/year
+        </span>
+      </div>
+      <a
+        href=""
+        className="text-[#382AE1] text-[13px] hover:cursor-pointer hover:text-[#766CF1] font-bold pr-[35px] underline underline-offset-1"
+      >
+        Change
+      </a>
+    </div>
+  );
 }
 
-
-
-export default  Price
+export default Price;
